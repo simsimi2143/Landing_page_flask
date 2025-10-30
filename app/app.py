036395a5e9ac6e7e28c141f285692bb2c12395c7
paulcitronico <paulcitronico@gmail.com>
2025-10-29 21:46:14 -0300

    escucha en todas las interfaces de red

--- app/app.py
+++ app/app.py
@@ -460,4 +460,4 @@ def eliminar_archivo_concurso(id):
     return redirect(url_for('editar_concurso', id=concurso_id))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
